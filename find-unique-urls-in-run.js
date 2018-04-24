d56c8ec58f793f25572bd6f8d7aa6d04e1f942a5
tannm@neoscorp.vn <neostan@neoss-Mac-mini.local>
2018-04-24 18:05:03 +0700

    throw error

--- find-unique-urls-in-run.js
+++ find-unique-urls-in-run.js
@@ -4,11 +4,14 @@
  * This sample code illustrates how to read a collection JSON file in NodeJS,run it using Newman and then log all the
  * unique URLs that were requested.
  */
-var newman = require('../'), // require('newman')
+var newman = require('newman'),
     uniqueUrls = {}; // here we will maintain the unique URLs
 
 // call newman.run to pass `options` object and listen to events
-newman.run({ collection: require('./sample-collection.json') })
+newman.run({ 
+    collection: require('./sample-collection.json'),
+    reporters: 'cli' 
+    })
     .on('start', function (err, args) {
         if (err) { return; }
 
@@ -27,12 +30,19 @@ newman.run({ collection: require('./sample-collection.json') })
             uniqueUrls[url] = 1; // otherwise start a new counter
         }
     })
-    .once('done', function (err) {
+    .on('done', function (err, summary) {
         var urls = Object.keys(uniqueUrls); // get list of all unique urls as an array from the object hash
 
         // now output the result to console
-        console.info(`The collection run completed ${err ? 'with' : 'without'} error(s).`);
         console.info(`Total ${urls.length} unique URLs requested.`);
+        var failures = summary.run.failures;
+        if (failures) {
+            console.error('collection run encountered an error.');
+            throw new Error ('collection run encountered an error.')
+        }
+        else {
+            console.info(`The collection run completed ${err ? 'with' : 'without'} error(s).`);
+        }
 
         urls.forEach(function (url) {
             console.info(`${uniqueUrls[url]}: ${url}`);
