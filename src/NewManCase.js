a01789b33f518d7c5d446d401519737e1928adda
tannm@neoscorp.vn <neostan@neoss-Mac-mini.local>
2018-04-26 11:25:31 +0700

    try remove mocha test

--- src/NewManCase.js
+++ src/NewManCase.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+var expect = require("chai").expect;
 var handler = require("./NewManRunner");
 handler.runPostManCollection("sample-collection", function(result) {
     console.log(result);
+    expect(result).to.equal("The sample-collection run completed with result pass.")
 });
